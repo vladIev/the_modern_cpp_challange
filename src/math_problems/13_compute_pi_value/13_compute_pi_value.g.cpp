38182a310a77df61352d7bb28cf3a446787140ca
vlad.levotskiy <vlad.levotskiy@gmail.com>
2023-07-01 11:08:55 +0300

    Fix formatting in task 13

--- src/math_problems/13_compute_pi_value/13_compute_pi_value.g.cpp
+++ src/math_problems/13_compute_pi_value/13_compute_pi_value.g.cpp
@@ -3,10 +3,10 @@
 #include <random>
 #include <gtest/gtest.h>
 
-TEST(compute_pi, two_digits_test){
+TEST(compute_pi, two_digits_test)
+{
     std::random_device rd;
     std::mt19937 mt(42);
     std::uniform_real_distribution<double> dist(0.0, 1.0);
     EXPECT_DOUBLE_EQ(3.14, std::round(computePi(mt, dist) * 100) / 100);
 }
-
--- src/math_problems/13_compute_pi_value/compute_pi.hpp
+++ src/math_problems/13_compute_pi_value/compute_pi.hpp
@@ -1,15 +1,17 @@
 #include <random>
 
-template<typename RandomEngine=std::mt19937, typename Distribution=std::uniform_real_distribution<>>
-double computePi(RandomEngine& engine, Distribution& distribution, uint32_t samples=1'000'000){
+template<typename RandomEngine = std::mt19937,
+    typename Distribution = std::uniform_real_distribution<>>
+double computePi(RandomEngine &engine,
+    Distribution &distribution,
+    uint32_t samples = 1'000'000)
+{
     auto inCircleSamplesCntr = 0u;
 
-    for (auto i = 0; i < samples; i++){
+    for (auto i = 0; i < samples; i++) {
         auto x = distribution(engine);
         auto y = distribution(engine);
-        if (y * y <= 1 - x * x){
-            inCircleSamplesCntr++;
-        }
+        if (y * y <= 1 - x * x) { inCircleSamplesCntr++; }
     }
 
     return 4.0 * inCircleSamplesCntr / samples;
