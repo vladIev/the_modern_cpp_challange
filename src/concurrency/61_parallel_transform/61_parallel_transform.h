cda3b16aa560c9a797c4ecc1d3f859a088bdf416
Владислав Левоцкий <vlad.levotskiy@gmail.com>
2023-07-09 19:05:32 +0300

    clang-tidy chapter 6

--- src/concurrency/61_parallel_transform/61_parallel_transform.h
+++ src/concurrency/61_parallel_transform/61_parallel_transform.h
@@ -2,43 +2,42 @@
 #include <vector>
 #include <future>
 
-auto get_num_of_threads(int64_t length, int64_t min_chunk_size = 32)
+auto getNumOfThreads(int64_t length, int64_t minChunkSize = 32)
 {
-    const uint32_t max_threads =
-        static_cast<uint32_t>((length + min_chunk_size - 1) / min_chunk_size);
-    const uint32_t hardware_threads = std::thread::hardware_concurrency();
-    return std::min(
-        hardware_threads != 0u ? hardware_threads : 2u, max_threads);
+    const auto maxThreads =
+        static_cast<uint32_t>((length + minChunkSize - 1) / minChunkSize);
+    const uint32_t hardwareThreads = std::thread::hardware_concurrency();
+    return std::min(hardwareThreads != 0U ? hardwareThreads : 2U, maxThreads);
 }
 
 template<typename Iterator, typename Func>
-auto ptransform(Iterator begin, Iterator end, Func f) -> void
+auto ptransform(Iterator begin, Iterator end, Func transformFunc) -> void
 {
     const auto length = std::distance(begin, end);
     if (!length) { return; }
 
-    const auto num_threads = get_num_of_threads(length);
-    const auto chunk_size = length / num_threads;
+    const auto numThreads = getNumOfThreads(length);
+    const auto chunkSize = length / numThreads;
 
-    std::vector<std::future<void>> futures(num_threads - 1);
-    std::vector<std::thread> threads(num_threads - 1);
-    
-    Iterator chunk_start = begin;
-    for (auto i = 0u; i < num_threads - 1; i++) {
-        Iterator chunk_end = chunk_start;
-        std::advance(chunk_end, chunk_size);
+    std::vector<std::future<void>> futures(numThreads - 1);
+    std::vector<std::thread> threads(numThreads - 1);
 
-        std::packaged_task<void(void)> task(
-            [=]() { std::transform(chunk_start, chunk_end, chunk_start, f); });
+    Iterator chunkStart = begin;
+    for (auto i = 0U; i < numThreads - 1; i++) {
+        Iterator chunkEnd = chunkStart;
+        std::advance(chunkEnd, chunkSize);
+
+        std::packaged_task<void(void)> task([=]() {
+            std::transform(chunkStart, chunkEnd, chunkStart, transformFunc);
+        });
 
         futures[i] = task.get_future();
         threads[i] = std::thread(std::move(task));
-        chunk_start = chunk_end;
+        chunkStart = chunkEnd;
     }
 
-    std::transform(chunk_start, end, chunk_start, f);
+    std::transform(chunkStart, end, chunkStart, transformFunc);
 
     for (auto &future : futures) { future.get(); }
     for (auto &thread : threads) { thread.join(); }
-
 }
--- src/concurrency/README.md
+++ src/concurrency/README.md
@@ -1,5 +1,8 @@
 ## Tasks
-### 1. Parallel transfrom algorithm
+### [1. Parallel transfrom algorithm](https://github.com/vladIev/the_modern_cpp_challange/tree/main/src/concurrency/61_parallel_transform)
 
+### [2. Parallel min and max search using std::threads](https://github.com/vladIev/the_modern_cpp_challange/tree/main/src/concurrency/62_parallel_minmax_threads)
+
+### [3. Parallel min and max search using std::async](https://github.com/vladIev/the_modern_cpp_challange/tree/main/src/concurrency/63_parallel_minmax_async)
 
 
